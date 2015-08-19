e57b4eabc19b24e3e2e2ea91c714f2975d1645c5
James White <cyberyt@gmail.com>
2015-08-19 20:13:15 +0100

    can't refactor withdraw function to be dynamic so creating balanceProtection() instead

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
-  console.log('Good evening Mr Phelps');
   setup();
 });
+
+var currentAccBalance = 0;
+var savingsAccBalance = 0;
 
 function setup() {
   inTheRed();
   $('input#deposit1').on('click', depositCurrent);
   $('input#deposit2').on('click', depositSavings);
   $('input#withdraw1').on('click', withdrawCurrent);
-  $('input#withdraw2').on('click', withdrawSavings);
+  // $('input#withdraw2').on('click', withdrawSavings);
 }
-
-var currentAccBalance = 0;
-var savingsAccBalance = 0;
 
 function inTheRed() {
   if (currentAccBalance === 0) {
@@ -28,8 +27,27 @@ function depositCurrent() {
   var input = $('input#amount1').val(); //grabs user input from box
   currentAccBalance = (currentAccBalance+parseFloat(input)); // adds it the global variable.
   $('#balance1').html('$'+currentAccBalance); //updates the display value.
-  inTheRed(); // checks if in the red.  
+  inTheRed(); // checks if in the red.   
 }
+
+function withdrawCurrent() {
+  var input = $('input#amount1').val();
+  currentAccBalance = (currentAccBalance-parseFloat(input));
+  if (currentAccBalance < 0) {
+    balanceProtection(Math.abs(currentAccBalance)); // Sends the 'overdraft' amount to balance protection
+    currentAccBalance = currentAccBalance + Math.abs(currentAccBalance); // puts balance back to 0.
+    $('#balance1').html('$'+currentAccBalance); //updates the display value.
+    inTheRed(); // checks if in the red/0.
+  } else { 
+  $('#balance1').html('$'+currentAccBalance); //updates the display value.
+  inTheRed(); // checks if in the red/0.
+  }
+}
+
+function balanceProtection(overdraft){
+  console.log(overdraft);
+}
+
 
 function depositSavings() {
   var input = $('input#amount2').val(); //grabs user input from box
@@ -37,17 +55,13 @@ function depositSavings() {
   $('#balance2').html('$'+savingsAccBalance);
 }
 
-function withdrawCurrent() {
-  var input = $('input#amount1').val();
-  currentAccBalance = (currentAccBalance-parseFloat(input));
-  if (currentAccBalance < 0) {
-    withdrawSavings(Math.abs(currentAccBalance));
 
-  } else {
-  $('#balance1').html('$'+currentAccBalance); //updates the display value.
-  inTheRed(); // checks if in the red/0.
-  }
-}
+
+// function depositSavings() {
+//   var input = $('input#amount2').val(); //grabs user input from box
+//   savingsAccBalance = (savingsAccBalance+parseFloat(input)); // adds it the global variable.
+//   $('#balance2').html('$'+savingsAccBalance);
+// }
 
 
 
