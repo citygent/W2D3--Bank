b9cd9d362ccaf9d6bf3fc83d87181cb35be19f74
James White <cyberyt@gmail.com>
2015-08-19 18:35:05 +0100

    inTheRed added for current

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 });
 
 function setup() {
+  inTheRed();
   $('input#deposit1').on('click', depositCurrent);
   $('input#deposit2').on('click', depositSavings);
   $('input#withdraw1').on('click', withdrawCurrent);
@@ -13,17 +14,26 @@ function setup() {
 var currentAccBalance = 0;
 var savingsAccBalance = 0;
 
+function inTheRed() {
+  if (currentAccBalance === 0) {
+    $('#balance1').css({"background-color": "#F52F4F"});
+    $('#account1.account').css({"background-color": "#FFFFFF"});
+  } else {
+    $('#balance1').css({"background-color": "#E3E3E3"});
+    $('#account1.account').css({"background-color": "#6C9A74"});    
+  }   
+}
+
 function depositCurrent() {
   var input = $('input#amount1').val(); //grabs user input from box
   currentAccBalance = (currentAccBalance+parseFloat(input)); // adds it the global variable.
-  //update the display.
-  $('#balance1').html('$'+currentAccBalance);
+  $('#balance1').html('$'+currentAccBalance); //updates the display value.
+  inTheRed(); // checks if in the red.  
 }
 
 function depositSavings() {
   var input = $('input#amount2').val(); //grabs user input from box
   savingsAccBalance = (savingsAccBalance+parseFloat(input)); // adds it the global variable.
-  //update the display.
   $('#balance2').html('$'+savingsAccBalance);
 }
 
